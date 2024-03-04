ceaef7f1acae143dd2298000a359b2d02beb83cb
Nibedita <nibeditarana7@gmail.com>
2024-03-04 17:04:28 +0530

    file and folder set

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./Pages/Amazonpage/Home";
+import Gallary from "./Pages/Amazonpage/Gallary";
+function App() {
+  return (
+    <div className="App">
+      {/* <Home /> */}
+      <Gallary/>
+    </div>
+  );
+}
+
+export default App;
